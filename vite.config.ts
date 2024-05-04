53dac67e8bc6b49f10084fb1097fea8ba4dbfea5
lodmev <lodmev@gmail.com>
2024-05-05 00:18:21 +0300

    feat: add polyfill for nodejs functions in browser For correct work sdk

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
   },
   envPrefix: ['API'],
 });
