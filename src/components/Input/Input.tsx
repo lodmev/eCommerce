ba2a029a868593c844215e4be1b09296059bdf16
AndyGuit <totalslash@msn.com>
2024-05-04 23:11:05 +0300

    feat: add useValidateInputHook

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,24 +2,29 @@ import { ChangeEvent } from 'react';
 import styles from './Input.module.css';
 
 type Props = {
+  errorText?: string;
   type?: 'text' | 'password' | 'email' | 'date';
   placeholder?: string;
-  invalid?: 'true';
+  invalid?: boolean;
+  value?: string;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: () => void;
 };
 
 export default function Input(props: Props) {
-  const { onChange, placeholder, type, invalid } = props;
+  const { onChange, value, onBlur, errorText, placeholder, type, invalid } = props;
 
   return (
     <div className={styles['input-wrapper']}>
       <input
+        value={value}
         className={`${styles.input} ${invalid ? styles.invalid : ''}`}
         onChange={onChange}
+        onBlur={onBlur}
         placeholder={placeholder}
         type={type}
       />
-      {invalid && <p className={styles.invalid}>invalid value</p>}
+      {invalid && <p className={styles['error-text']}>{errorText}</p>}
     </div>
   );
 }
--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -2,16 +2,34 @@ import { FormEvent } from 'react';
 import Button from '../../components/Button/Button';
 import FormRegistration from '../../components/FormRegistration/FormRegistration';
 import Input from '../../components/Input/Input';
+import useValidateInput from '../../hooks/useValidateInput';
+import { validateEmail } from '../../utils/functions';
 
 export default function Registration() {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
 
+  const {
+    value: emailInputValue,
+    // isValid: emailIsValid,
+    hasError: emailHasError,
+    inputBlurHandler: emailBlurHandler,
+    valueChangeHandler: emailChangeHandler,
+  } = useValidateInput(validateEmail);
+
   return (
     <div>
       <FormRegistration onSumbit={handleSubmit}>
-        <Input type="email" placeholder="Email" />
+        <Input
+          onBlur={emailBlurHandler}
+          onChange={emailChangeHandler}
+          value={emailInputValue}
+          invalid={emailHasError}
+          type="email"
+          placeholder="Email"
+          errorText="Invalid email address"
+        />
         <Button type="submit" styleClass="green-filled">
           Register
         </Button>
