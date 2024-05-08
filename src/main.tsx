86f5fd620b0f30f46697597f1051b1b8e86c7c7f
VictoriaGorobets <vickyhome1996@gmail.com>
2024-05-08 22:52:34 +0300

    feat: wire up components for Main page

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './Variables.module.css';
 
 const root = document.createElement('div');
 root.id = 'root';
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,3 +1,13 @@
+import Description from '../../components/Description/Description';
+import Catalog from '../../components/Catalog/Catalog';
+import ProductOfTheMonth from '../../components/ProductOfTheMonth/ProductOfTheMonth';
+
 export default function Main() {
-  return <div>Main page</div>;
+  return (
+    <>
+      <Description />
+      <Catalog />
+      <ProductOfTheMonth />
+    </>
+  );
 }
