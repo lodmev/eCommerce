cca3af9aae4ffa2a31d3861855a5b2befb486b53
lodmev <lodmev@gmail.com>
2024-05-10 10:45:17 +0300

    fix: move root css variables

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './Variables.module.css';
 
 const root = document.createElement('div');
 root.id = 'root';
