2083ab6fdd58618e701d40caca9bc0f868288d31
lodmev <lodmev@gmail.com>
2024-05-11 18:00:04 +0300

    fix: change method to function

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,17 +1,17 @@
 const logger = window.console;
+
+const executeIfDev = (func: () => void) => {
+  if (import.meta.env.DEV) func();
+};
 const debug = {
-  executeIfDev(func: () => void) {
-    if (import.meta.env.DEV) func();
-  },
   log(...data: unknown[]) {
-    this.executeIfDev(() => logger.log(...data));
+    executeIfDev(() => logger.log(...data));
   },
   error(...data: unknown[]) {
-    this.executeIfDev(() => logger.error(...data));
+    executeIfDev(() => logger.error(...data));
   },
   dir(...data: unknown[]) {
-    this.executeIfDev(() => logger.dir(...data));
+    executeIfDev(() => logger.dir(...data));
   },
 };
-
 export default debug;
