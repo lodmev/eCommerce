c7418c02e33f10f0cf150dd04ade267491957250
lodmev <lodmev@gmail.com>
2024-05-13 10:58:08 +0300

    refactor: some small style changes
    : title

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -4,7 +4,7 @@ import styles from './Basket.module.css';
 export default function Basket() {
   return (
     <div className={styles.cart}>
-      <h2>You have not add products to cart yet</h2>
+      <h2>{`You haven't added items to your cart yet`}</h2>
       <HashLink to="/#catalog">Go to catalogue</HashLink>
     </div>
   );
