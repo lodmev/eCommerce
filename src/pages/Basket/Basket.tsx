ac0c5ef3d85a0939561ef7c2c26b1d87c5219ac1
lodmev <lodmev@gmail.com>
2024-05-13 10:57:22 +0300

    feat: add some styles to basket page

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -1,3 +1,11 @@
+import { HashLink } from 'react-router-hash-link';
+import styles from './Basket.module.css';
+
 export default function Basket() {
-  return <div>Basket page</div>;
+  return (
+    <div className={styles.cart}>
+      <h2>You have not add products to cart yet</h2>
+      <HashLink to="/#catalog">Go to catalogue</HashLink>
+    </div>
+  );
 }
