ff71df0b7c70ee6c5a4c8541f13f9548fe06c180
AndyGuit <totalslash@msn.com>
2024-05-20 03:32:57 +0300

    feat: display user information

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,3 +1,4 @@
+import { Customer } from '@commercetools/platform-sdk';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getCurrentCustomer } from '../../api/customers';
@@ -10,6 +11,7 @@ export default function UserProfile() {
   const navigate = useNavigate();
   const { isUserAuthorized } = useStoreSelector((state) => state.userData);
   const [isLoading, setIsLoading] = useState(true);
+  const [customer, setCustomer] = useState<null | Customer>(null);
 
   useEffect(() => {
     if (!isUserAuthorized) {
@@ -21,7 +23,7 @@ export default function UserProfile() {
     async function getUser() {
       const user = await getCurrentCustomer();
       setIsLoading(false);
-      return user;
+      setCustomer(user);
     }
 
     getUser();
@@ -34,5 +36,37 @@ export default function UserProfile() {
       </Overlay>
     );
 
-  return <div>UserProfile page</div>;
+  if (!customer) return null;
+
+  return (
+    <div>
+      <h1>
+        Hello, {customer.firstName} {customer.lastName}
+      </h1>
+      <p>Date of birth: {customer.dateOfBirth}</p>
+      <ul>
+        Addresses:
+        {customer.addresses.map((address) => (
+          <li key={address.id}>
+            {address.id &&
+              customer.shippingAddressIds?.includes(address.id) &&
+              'Shipping Address: '}
+            {address.id && customer.billingAddressIds?.includes(address.id) && 'Billing Address: '}
+            <br />
+            Country: {address.country}
+            <br />
+            City: {address.city}
+            <br />
+            Street Name: {address.streetName}
+            <br />
+            Postal Code: {address.postalCode}
+            <br />
+            {address.id === customer.defaultShippingAddressId && 'This is Default Shipping Address'}
+            {address.id === customer.defaultBillingAddressId && 'This is Default Billing Address'}
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
