566fd7ba2e2c1894172d184baa010e8eeb80e38e
AndyGuit <totalslash@msn.com>
2024-05-20 19:11:53 +0300

    feat: show loading spinner while going on userProfile page

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,12 +1,17 @@
 import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import { getCurrentCustomer } from '../api/customers';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
+import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner';
+import Overlay from '../components/Modal/Overlay';
 import debug from '../utils/debug';
 import { getToken } from '../utils/token';
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   useEffect(() => {
     if (!getToken()) return;
 
@@ -14,8 +19,14 @@ export default function AppLayout() {
       .then((customer) => debug.log('customer: ', customer))
       .catch(debug.error);
   }, []);
+
   return (
     <div className="container">
+      {isLoading && (
+        <Overlay>
+          <LoadingSpinner />
+        </Overlay>
+      )}
       <Header />
       <main>
         <Outlet />
