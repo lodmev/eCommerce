bdeaa6061e3c9abec967dcb91486abcc5b5c8b17
lodmev <lodmev@gmail.com>
2024-05-29 23:56:22 +0300

    refactor: add default sorting by product name change type of search parameters

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -3,11 +3,15 @@ import { Link, Params, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import styles from './Catalog.module.css';
 import ProductCard from '../Products/ProductCard';
-import { PRODUCT_DEFAULT_FETCH_LIMIT, ROUTE_PATH } from '../../utils/globalVariables';
+import {
+  DEFAULT_LANGUAGE_KEY,
+  PRODUCT_DEFAULT_FETCH_LIMIT,
+  ROUTE_PATH,
+} from '../../utils/globalVariables';
 import { useStoreSelector } from '../../hooks/userRedux';
 import useAsync from '../../hooks/useAsync';
 import { getProductCategoriesMap, searchProducts } from '../../api/products';
-import { SearchProductsQuery } from '../../types/types';
+import { QueryArgs } from '../../types/types';
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
 import Loader from '../Modal/Loader';
 // import debug from '../../utils/debug';
@@ -16,20 +20,23 @@ const getID = (params?: Readonly<Params<string>>) => params?.subCatID || params?
 
 const findProducts = async (params?: Readonly<Params<string>>) => {
   const id: string = getID(params);
-  const queryArgs: { filter?: string | string[]; limit: number } = {
+  const queryArgs: QueryArgs = {
     limit: PRODUCT_DEFAULT_FETCH_LIMIT,
   };
   if (id) {
     queryArgs.filter = `categories.id:"${id}"`;
   }
-
-  const searchQuery: SearchProductsQuery = { queryArgs };
-  return searchProducts(searchQuery);
+  if (queryArgs.sort && queryArgs.sort instanceof Array) {
+    queryArgs.sort.push('"createdAt"');
+  } else {
+    queryArgs.sort = [`name.${DEFAULT_LANGUAGE_KEY} asc`];
+  }
+  return searchProducts(queryArgs);
 };
 
 export default function Catalog({ withLink }: { withLink?: boolean }) {
   const defaultCategoryName = 'All categories';
-  const [currentCategory, setCurrentCategory] = useState(defaultCategoryName);
+  const [currentCategory, setCurrentCategory] = useState('Catalog');
   const params = useParams();
   const [needUpdate, setNeedUpdate] = useState(false);
   const [allProductsResponse, isLoading, err] = useAsync(findProducts, params, [
