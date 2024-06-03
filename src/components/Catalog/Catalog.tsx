906a47861ac5f4e0a0f88b7834e75a1ea72cf2aa
lodmev <lodmev@gmail.com>
2024-06-03 12:53:54 +0300

    refactor: move doProductSearchRequest function to separate file

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,32 +1,18 @@
-import { ProductProjection } from '@commercetools/platform-sdk';
-import { Link, Params, useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { ProductProjection } from '@commercetools/platform-sdk';
 import styles from './Catalog.module.css';
-import ProductCard from '../Products/ProductCard';
-import {
-  CATALOG_PREVIEW_LIMIT,
-  PRODUCT_DEFAULT_FETCH_LIMIT,
-  ROUTE_PATH,
-} from '../../utils/globalVariables';
+import { CATALOG_PREVIEW_LIMIT, ROUTE_PATH } from '../../utils/globalVariables';
 import { useStoreSelector } from '../../hooks/userRedux';
 import useAsync from '../../hooks/useAsync';
-import { getProductCategoriesMap, searchProducts } from '../../api/products';
-import { QueryArgs } from '../../types/types';
+import { getProductCategoriesMap } from '../../api/products';
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
 import Loader from '../Modal/Loader';
 import Filters from './Filters/Filters';
 import Sorting from './Sorting/Sorting';
 import Searching from './Searching/Searching';
-// import debug from '../../utils/debug';
-
-type RouteParams = Readonly<Params<string>>;
-
-type RequestParams = {
-  routeParams?: RouteParams;
-  searchParams: URLSearchParams;
-  sortParams: string;
-  searchRequest: { locale: string; value: string };
-};
+import { doSearchRequest, getID } from '../../api/doProductSearchRequest';
+import ProductCard from '../Products/ProductCard';
 
 function ProductCards({
   isPreview,
@@ -49,69 +35,6 @@ function ProductCards({
       ))
     : null;
 }
-const getID = (routeParams?: RouteParams) => routeParams?.subCatID || routeParams?.catID || '';
-
-const appendFilters = ({
-  locationParams,
-  queryArgs,
-}: {
-  locationParams: Omit<RequestParams, 'sortParams' | 'searchRequest'>;
-  queryArgs: QueryArgs;
-}) => {
-  const id: string = getID(locationParams.routeParams);
-  if (!id && locationParams.searchParams.size === 0) {
-    return queryArgs;
-  }
-
-  const filterQuery: string[] = [];
-  queryArgs['filter.query'] = filterQuery;
-
-  if (id) {
-    filterQuery.push(`categories.id:"${id}"`);
-  }
-  // const priceRange = locationParams.searchParams.get('f_price');
-  locationParams.searchParams.forEach((value, key) => {
-    if (key.startsWith('f_')) filterQuery.push(value);
-  });
-  return queryArgs;
-};
-
-const getQuery = ({
-  routeParams,
-  searchParams,
-  sortParams,
-  searchRequest: searchText,
-}: RequestParams): QueryArgs => {
-  const queryArgs: QueryArgs = {
-    limit: PRODUCT_DEFAULT_FETCH_LIMIT,
-  };
-  // add search text
-  if (searchText.value) {
-    queryArgs[`text.${searchText.locale}`] = searchText.value;
-    queryArgs.fuzzy = true;
-  }
-  // append filters
-  appendFilters({ locationParams: { routeParams, searchParams }, queryArgs });
-  // set sort params
-  if (sortParams === '') {
-    queryArgs.sort = [`score asc`];
-    queryArgs.sort.push('id asc');
-  } else {
-    queryArgs.sort = [sortParams];
-  }
-  return queryArgs;
-};
-
-const doSearchRequest = async ({
-  routeParams,
-  searchParams,
-  sortParams,
-  searchRequest: searchText,
-}: RequestParams) => {
-  const query = getQuery({ routeParams, searchParams, sortParams, searchRequest: searchText });
-  return searchProducts(query);
-};
-
 export default function Catalog({ isPreview }: { isPreview?: boolean }) {
   const defaultCategoryName = 'All categories';
   const [currentCategory, setCurrentCategory] = useState('Catalog');
