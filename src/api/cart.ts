d28605032e7023e7ec197b4ff414f6832c0f7b81
lodmev <lodmev@gmail.com>
2024-06-06 12:27:03 +0300

    feat: implement functions for fetch cart

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,2 +1,22 @@
-export const createCart = () => {};
+import { getCurrentApiClient } from './apiRoot';
+
+export const createCart = async () => {
+  const resp = await getCurrentApiClient()
+    .me()
+    .carts()
+    .post({ body: { currency: 'EUR' } })
+    .execute();
+  return resp.body;
+};
 export const addToCart = () => {};
+export const getActiveCart = async () => {
+  try {
+    const resp = await getCurrentApiClient().me().activeCart().get().execute();
+    return resp.body;
+  } catch (e) {
+    if (e instanceof Object && 'statusCode' in e) {
+      return createCart();
+    }
+    throw e;
+  }
+};
