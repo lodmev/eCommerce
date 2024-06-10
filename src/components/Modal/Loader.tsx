ca927ca8cba94e5834869f8bc0439a2875a5f090
lodmev <lodmev@gmail.com>
2024-06-10 16:06:19 +0300

    feat: add default confirm handler to Modal component

--- src/components/Modal/Loader.tsx
+++ src/components/Modal/Loader.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import ModalAlert from './ModalAlert';
 import Overlay from './Overlay';
+import { ROUTE_PATH } from '../../utils/globalVariables';
 
 export default function Loader({
   isLoading,
@@ -11,10 +13,16 @@ export default function Loader({
   errMsg?: string;
   errorHandler?: () => void;
 }) {
+  const navigate = useNavigate();
+  const defaultHandler = () => {
+    navigate(ROUTE_PATH.main);
+  };
   return isLoading || errMsg ? (
     <Overlay>
       {isLoading && <LoadingSpinner />}
-      {errMsg && <ModalAlert message={`${errMsg}`} isError onConfirm={errorHandler} />}
+      {errMsg && (
+        <ModalAlert message={`${errMsg}`} isError onConfirm={errorHandler ?? defaultHandler} />
+      )}
     </Overlay>
   ) : null;
 }
