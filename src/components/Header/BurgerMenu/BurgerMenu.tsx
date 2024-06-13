5b2c6271bbadc7e22dc0abb5289c614c2f7b98b3
AndyGuit <totalslash@msn.com>
2024-06-13 23:06:16 +0300

    fix: navigate to about us page from burger menu

--- src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -57,7 +57,7 @@ const NavigationPathToIcon = new Map<NavigationPath, IconDefinition>([
 ]);
 
 const NavigationPathToPath = new Map<NavigationPath, string>([
-  [NavigationPath.About, '/#about'],
+  [NavigationPath.About, ROUTE_PATH.about],
   [NavigationPath.Catalog, '/products'],
   [NavigationPath.ProductOfTheMonth, '/#productOfTheMonth'],
   [NavigationPath.Contacts, '/#contacts'],
