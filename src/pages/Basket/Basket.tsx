896af64e329c45ca5effea292365ad97d6d8e1e7
lodmev <lodmev@gmail.com>
2024-06-14 10:36:36 +0300

    fix: style of actions buttons in cart

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -17,16 +17,6 @@ import ModalConfirm from '../../components/Modal/ModalConfirm';
 const clearCartConfirmMessage: string = 'Are you sure you want to delete all items in the cart?';
 
 export default function Basket() {
-  // const { productsInBasket, productIdToQuantity } = useStoreSelector((state) => state.basketData);
-  // let currency = '';
-  // const subTotalPrice = productsInBasket.reduce((subTotalPrice, product) => {
-  //   const { id, masterVariant } = product;
-  //   const priceHelper = new PriceHelper({ price: masterVariant.prices![0] });
-  //   const { finalPriceValue } = priceHelper;
-  //   currency = currency || priceHelper.currency;
-  //   const quantity = productIdToQuantity[id];
-  //   return subTotalPrice + calculateTotalPrice(+finalPriceValue, quantity);
-  // }, 0);
   const dispatch = useStoreDispatch();
   const { cartData, pending, err } = useStoreSelector((state) => state.basketData);
   useEffect(() => {
