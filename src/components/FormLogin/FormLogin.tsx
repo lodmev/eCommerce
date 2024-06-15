9ebe9b2609e9e6684c793ed1564803c4b6029a8a
lodmev <lodmev@gmail.com>
2024-06-15 21:39:56 +0300

    refactor: change validation behavior on login and registration

--- src/components/FormLogin/FormLogin.tsx
+++ src/components/FormLogin/FormLogin.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
 import { MyCustomerSignin } from '@commercetools/platform-sdk';
 import { Link } from 'react-router-dom';
 import { validateEmail, validatePassword } from '../../utils/functions';
@@ -6,6 +6,7 @@ import Button from '../Button/Button';
 import Input from '../Input/Input';
 import styles from './FormLogin.module.css';
 import { ROUTE_PATH } from '../../utils/globalVariables';
+// import debug from '../../utils/debug';
 
 type Props = {
   onSubmit: (loginData: MyCustomerSignin) => void;
@@ -16,33 +17,41 @@ export default function FormLogin(props: Props) {
 
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
-  const [emailIsTouched, setEmailIsTouched] = useState(false);
-  const [passwordIsTouched, setPasswordIsTouched] = useState(false);
+  // const [emailIsTouched, setEmailIsTouched] = useState(false);
+  // const [passwordIsTouched, setPasswordIsTouched] = useState(false);
   const [emailIsValid, setEmailIsValid] = useState(true);
   const [passwordIsValid, setPasswordIsValid] = useState(true);
-  const [formIsValid, setFormIsValid] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(true);
 
   const handleChangeEmail = (email: string) => {
     setEmailValue(email);
     setEmailIsValid(validateEmail(email));
-    setEmailIsTouched(true);
-    setFormIsValid(validateEmail(email) && passwordIsValid && emailIsTouched && passwordIsTouched);
+    setFormIsValid(validateEmail(email) && passwordIsValid);
   };
 
   const handleChangePassword = (password: string) => {
     setPasswordValue(password);
     setPasswordIsValid(validatePassword(password));
-    setPasswordIsTouched(true);
-    setFormIsValid(
-      emailIsValid && validatePassword(password) && emailIsTouched && passwordIsTouched,
-    );
+    setFormIsValid(validatePassword(password) && emailIsValid);
   };
-
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  useEffect(() => {
+    if (loginRef.current) loginRef.current.focus();
+  }, []);
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const isEmailValid = validateEmail(emailValue);
+    const isPasswordValid = validatePassword(passwordValue);
+    if (!isEmailValid) {
+      setEmailIsValid(false);
+      setFormIsValid(false);
+    }
 
-    if (!formIsValid) return;
-
+    if (!isPasswordValid) {
+      setPasswordIsValid(false);
+      setFormIsValid(false);
+    }
+    if (!isEmailValid || !isPasswordValid) return;
     const userData = {
       email: emailValue,
       password: passwordValue,
@@ -56,18 +65,17 @@ export default function FormLogin(props: Props) {
         <Input
           onChange={(e: ChangeEvent<HTMLInputElement>) => handleChangeEmail(e.target.value)}
           value={emailValue}
-          onBlur={() => setEmailIsTouched(true)}
           invalid={!emailIsValid}
           id="email"
           label="Your Email"
           type="text"
           placeholder="Email"
           errorText="Invalid email address. Must not contain leading or trailing whitespace."
+          ref={loginRef}
         />
         <Input
           onChange={(e: ChangeEvent<HTMLInputElement>) => handleChangePassword(e.target.value)}
           value={passwordValue}
-          onBlur={() => setPasswordIsTouched(true)}
           invalid={!passwordIsValid}
           id="password"
           label="Your password"
